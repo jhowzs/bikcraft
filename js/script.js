f0768f4d3c466257b3ccace8310d9173bbc44ce1
Jonathan Marques <jhowmarques2501@gmail.com>
2023-01-09 09:51:51 -0300

    :feat: Menu JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("teste");
+// activate menu links
+const links = document.querySelectorAll(".header-menu a");
+
+function activateLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("activate");
+  }
+}
+
+links.forEach(activateLink);
